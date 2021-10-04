18c1ae59c08c49bf96c19405d93dd3700df4b4b6
valamidev <52665225+valamidev@users.noreply.github.com>
2021-10-04 19:28:09 +0200

    Add Telegram signal invalidation time

--- src/plugins/telegram/telegram.ts
+++ src/plugins/telegram/telegram.ts
@@ -1,7 +1,15 @@
-    import { EventEmitter } from 'eventemitter3';
+import { EventEmitter } from 'eventemitter3';
 import { Api, TelegramClient } from 'telegram';
 import { StringSession } from 'telegram/sessions';
 import Logger from '../../util/logger';
+
+export const getCurrentTimeUTC = () => {
+
+  const tmLoc = new Date();
+
+  return Math.floor((tmLoc.getTime()) / 1000);
+}
+
 
 export class TelegramScrapper extends EventEmitter {
   private ready = true;
@@ -24,14 +32,14 @@ export class TelegramScrapper extends EventEmitter {
     this.stringSession = new StringSession(session);
     this.channelName = channelName;
 
-    if(this.channelName === ''){
+    if (this.channelName === '') {
       Logger.log('No telegram channel set, telegram plugin disabled!');
       return;
     }
 
     this.client = new TelegramClient(this.stringSession, this.apiId, this.apiHash, {
-        connectionRetries: 5,
-      });
+      connectionRetries: 5,
+    });
 
     // Every 2 Minute check
     this.listener = setInterval(async () => {
@@ -45,14 +53,14 @@ export class TelegramScrapper extends EventEmitter {
     this.ready = false;
 
     try {
-      if(!this.client){
+      if (!this.client) {
         return;
       }
 
-      if(!this.client.connected){
+      if (!this.client.connected) {
         await this.client.connect();
       }
-      
+
       const channelResult = await this.client.invoke(
         new Api.channels.GetFullChannel({
           channel: this.channelName,
@@ -61,17 +69,17 @@ export class TelegramScrapper extends EventEmitter {
 
       const lastMessage = (channelResult.fullChat as any).readInboxMaxId;
 
-      if(!this.lastProcessed){
+      if (!this.lastProcessed) {
         this.lastProcessed = lastMessage;
       }
 
-     // const unreadCount = (channelResult.fullChat as any).unreadCount;
+      // const unreadCount = (channelResult.fullChat as any).unreadCount;
 
       Logger.log('Telegram', `Last: ${lastMessage} , Last processed: ${this.lastProcessed}`);
 
       let message = ''
 
-      while(message !== undefined){
+      while (message !== undefined) {
 
         const getLastMessage = await this.client.invoke(
           new Api.channels.GetMessages({
@@ -84,22 +92,26 @@ export class TelegramScrapper extends EventEmitter {
 
         message = content;
 
-        if(content){
+        if (content) {
           Logger.log('Telegram signal', content, new Date());
-          this.lastProcessed+=1; 
-        }else{
+          this.lastProcessed += 1;
+        } else {
           break;
         }
 
+        // Drop signals older than 15 minutes
+        if(getCurrentTimeUTC()  > (getLastMessage as any)?.messages[0].date + 900){
+          Logger.log('Telegram signal too old', getCurrentTimeUTC(), (getLastMessage as any)?.messages[0].date);
+          break;
+        }
 
-        
         // CMC list signals
         if (content && content.includes('first pump')) {
 
           Logger.log('Telegram address found!', content, new Date());
 
-           const regex = /0x[a-fA-F0-9]{40}/;
-           const almostAddress = content.match(regex);
+          const regex = /0x[a-fA-F0-9]{40}/;
+          const almostAddress = content.match(regex);
 
           if (almostAddress[0] && this.lastSignal !== almostAddress[0]) {
             this.lastSignal = almostAddress[0];
@@ -113,12 +125,12 @@ export class TelegramScrapper extends EventEmitter {
         if (content && content.includes('poocoin.app')) {
 
           Logger.log('Telegram address found!', content, new Date());
-  
+
           const almostAddres = content.split('poocoin.app/tokens/');
-  
+
           // for e.g. 0xb6a6dcccba92905c34801e1458b0606e07bb3dd4
           const address = almostAddres[1].substring(0, 42);
-  
+
           if (this.lastSignal !== address) {
             this.lastSignal = address;
             this.emit('newSignal', address);
@@ -126,7 +138,7 @@ export class TelegramScrapper extends EventEmitter {
           }
         }
 
-       
+
       }
     } catch (error) {
       Logger.log('Telegram error, ', error);
