5e6043bea803fe39d9fca97a17e18c6131e44c07
T <52665225+valamidev@users.noreply.github.com>
2021-12-02 09:29:20 +0100

    Add Cronos/CronaSwap

--- src/contants.ts
+++ src/contants.ts
@@ -1,5 +1,5 @@
-import { BigNumber } from "bignumber.js";
-import Web3 from "web3";
+import { BigNumber } from 'bignumber.js';
+import Web3 from 'web3';
 
 export const databaseName = 'top100storage.db';
 
@@ -10,53 +10,53 @@ export const approveSafeInfinity = '21579208923731619542357098500868790785326998
 export const DustLimit = new BigNumber(Web3.utils.toWei('0.015', 'ether'));
 
 export const ethereumChains = [
-    
-    {
+  {
     id: 'binance',
     name: 'Binance Smart Chain (BSC)',
-    slug: "BSC",
+    slug: 'BSC',
     logo: './assets/bsc.png',
     scanLogo: './assets/bscscan.png',
-    router: "0x10ED43C718714eb63d5aA57B78B54704E256024E",
-    factory: "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73",
+    router: '0x10ED43C718714eb63d5aA57B78B54704E256024E',
+    factory: '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73',
     rcpAddress: 'https://bsc-dataseed1.ninicoin.io/',
-    defaultGas: "5000000000",
+    defaultGas: '5000000000',
     wCoin: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
     testContract: '0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c',
     contractExplorer: (address: string) => `https://bscscan.com/token/${address}`,
-    chartLink:  (address: string) => `https://poocoin.app/tokens/${address}`,
-    swapLink:  (address: string) => `https://poocoin.app/tokens/${address}`
+    chartLink: (address: string) => `https://poocoin.app/tokens/${address}`,
+    swapLink: (address: string) => `https://poocoin.app/tokens/${address}`,
   },
   {
     id: 'ethereum',
     name: 'Ethereum (ETH)',
-    slug: "ETH",
+    slug: 'ETH',
     logo: './assets/ether.png',
     scanLogo: './assets/etherscan.png',
-    router: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
-    factory: "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f",
+    router: '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D',
+    factory: '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f',
     rcpAddress: 'https://cloudflare-eth.com/',
-    defaultGas: "35000000000",
+    defaultGas: '35000000000',
     wCoin: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
     testContract: '0xF629cBd94d3791C9250152BD8dfBDF380E2a3B9c',
     contractExplorer: (address: string) => `https://etherscan.io/address/${address}`,
-    chartLink:  (address: string) =>  `https://ethplorer.io/address/${address}#chart=candlestick`,
-    swapLink:  (address: string) => `https://app.1inch.io/#/1/classic/swap/ETH/${address}`
-  }, {
+    chartLink: (address: string) => `https://ethplorer.io/address/${address}#chart=candlestick`,
+    swapLink: (address: string) => `https://app.1inch.io/#/1/classic/swap/ETH/${address}`,
+  },
+  {
     id: 'polygon',
     name: 'Polygon (MATIC)',
-    slug: "MATIC",
+    slug: 'MATIC',
     logo: './assets/matic.png',
     scanLogo: './assets/none.png',
-    router: "0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff",
-    factory: "0x5757371414417b8C6CAad45bAeF941aBc7d3Ab32",
+    router: '0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff',
+    factory: '0x5757371414417b8C6CAad45bAeF941aBc7d3Ab32',
     rcpAddress: 'https://rpc-mainnet.maticvigil.com/',
-    defaultGas: "5000000000",
+    defaultGas: '5000000000',
     wCoin: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270',
     testContract: '0xa8fcee762642f156b5d757b6fabc36e06b6d4a1a',
     contractExplorer: (address: string) => `https://polygonscan.com/address/${address}`,
-    chartLink:  (address: string) => `https://polygon.poocoin.app/tokens/${address}`,
-    swapLink:  (address: string) => `https://polygon.poocoin.app/tokens/${address}`
+    chartLink: (address: string) => `https://polygon.poocoin.app/tokens/${address}`,
+    swapLink: (address: string) => `https://polygon.poocoin.app/tokens/${address}`,
   },
   {
     id: 'fantom',
@@ -67,7 +67,7 @@ export const ethereumChains = [
     router: '0xf491e7b69e4244ad4002bc14e878a34207e38c29',
     factory: '0x152ee697f2e276fa89e96742e9bb9ab1f2e61be3',
     rcpAddress: 'https://rpc.ftm.tools',
-    defaultGas: "100000000000",
+    defaultGas: '100000000000',
     wCoin: '0x21be370d5312f44cb42ce377bc9b8a0cef1a4c83',
     testContract: '0xbf1f3d7266a1080bf448e428daa37eec6b05a8ed',
     contractExplorer: (address: string) => `https://ftmscan.com/address/${address}`,
@@ -75,53 +75,69 @@ export const ethereumChains = [
     swapLink: (address: string) => `https://spookyswap.finance/swap?outputCurrency=${address}`,
   },
   {
+    id: 'cronos',
+    name: 'Cronos',
+    slug: 'CRO',
+    logo: './assets/cro.png',
+    scanLogo: './assets/none.png',
+    router: '0xcd7d16fB918511BF7269eC4f48d61D79Fb26f918',
+    factory: '0x73A48f8f521EB31c55c0e1274dB0898dE599Cb11',
+    rcpAddress: 'https://rpc-cronos.crypto.org',
+    defaultGas: '5000000000',
+    wCoin: '0x5C7F8A570d578ED84E63fdFA7b1eE72dEae1AE23',
+    testContract: '0xccae06ec0787c07d7df5a60856c73a113fc7cf9a',
+    contractExplorer: (address: string) => `https://cronos.crypto.org/explorer/tokens/${address}`,
+    chartLink: (address: string) => `https://dexscreener.com/cronos/${address}`,
+    swapLink: (address: string) => `https://app.cronaswap.org/swap?outputCurrency=${address}`,
+  },
+  {
     id: 'cth',
     name: 'cTH (cheapEthereum)',
-    slug: "cTH",
+    slug: 'cTH',
     logo: './assets/cth.png',
     scanLogo: './assets/none.png',
-    router: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
-    factory: "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f",
-    rcpAddress: "https://rpc.cheapeth.org/rpc",
-    defaultGas: "5000000000",
+    router: '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D',
+    factory: '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f',
+    rcpAddress: 'https://rpc.cheapeth.org/rpc',
+    defaultGas: '5000000000',
     wCoin: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
     testContract: '0xa34273ce752f5a8e926fa213bb08728b2387e8f5',
-    contractExplorer: (address: string) => `http://expedition.cheapeth.org/address/${address}?rpcUrl=https%3A%2F%2Frpc.cheapeth.org%2Frpc`,
-    chartLink:  (address: string) => `https://cheapswap.io/#/swap?outputCurrency=${address}`,
-    swapLink:  (address: string) => `https://cheapswap.io/#/swap?outputCurrency=${address}`
+    contractExplorer: (address: string) =>
+      `http://expedition.cheapeth.org/address/${address}?rpcUrl=https%3A%2F%2Frpc.cheapeth.org%2Frpc`,
+    chartLink: (address: string) => `https://cheapswap.io/#/swap?outputCurrency=${address}`,
+    swapLink: (address: string) => `https://cheapswap.io/#/swap?outputCurrency=${address}`,
   },
-  
+
   {
     id: 'dth',
     name: 'devETH',
-    slug: "devETH",
+    slug: 'devETH',
     logo: './assets/deveth.png',
     scanLogo: './assets/none.png',
-    router: "0xab06aD5E30c039c0fb628fe8598e569Fb6B97fdc",
-    factory: "0x85fA4b9d57A6e27e2c60796893f2007e0a081FC8",
-    rcpAddress: "https://rpc.deveth.org/", 
-    defaultGas: "5000000000",
+    router: '0xab06aD5E30c039c0fb628fe8598e569Fb6B97fdc',
+    factory: '0x85fA4b9d57A6e27e2c60796893f2007e0a081FC8',
+    rcpAddress: 'https://rpc.deveth.org/',
+    defaultGas: '5000000000',
     wCoin: '0xe5092b565b5119fa0860b0016d9E4cCe9cAB210E',
     testContract: '0xe758616f5f4db08398d88a9cd309397de16cdcdd',
     contractExplorer: (address: string) => `https://explore.deveth.org/account/${address}`,
-    chartLink:  (address: string) => `https://www.devethswap.de/#/swap?outputCurrency=${address}`,
-    swapLink:  (address: string) => `https://www.devethswap.de/#/swap?outputCurrency=${address}`
+    chartLink: (address: string) => `https://www.devethswap.de/#/swap?outputCurrency=${address}`,
+    swapLink: (address: string) => `https://www.devethswap.de/#/swap?outputCurrency=${address}`,
   },
-   {
+  {
     id: 'polygon:ida',
     name: 'IdaMurni-(IDA)',
-    slug: "IDA",
+    slug: 'IDA',
     logo: 'https://raw.githubusercontent.com/IdaMurni/website/main/src/assets/img/ida_murni_master.png',
     scanLogo: './assets/none.png',
-    router: "0x33AfA1A1a9343b2dfdf9B8a0615bEfC0A4584B14",
-    factory: "0x8F8c3A7ccD2980b9A7b5cfadc5d1Cf638dc1427A",
-    rcpAddress: "https://rpc-mainnet.maticvigil.com/",
-    defaultGas: "5000000000",
+    router: '0x33AfA1A1a9343b2dfdf9B8a0615bEfC0A4584B14',
+    factory: '0x8F8c3A7ccD2980b9A7b5cfadc5d1Cf638dc1427A',
+    rcpAddress: 'https://rpc-mainnet.maticvigil.com/',
+    defaultGas: '5000000000',
     wCoin: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270',
     testContract: '0xccae06ec0787c07d7df5a60856c73a113fc7cf9a',
     contractExplorer: (address: string) => `https://polygonscan.com/token/${address}`,
-    chartLink:  (address: string) => `https://info.idamurni.de/#/token/${address}`,
-    swapLink:  (address: string) => `https://exchange.idamurni.de/#/swap?outputCurrency=${address}`
-  }  
-  
-  ];
+    chartLink: (address: string) => `https://info.idamurni.de/#/token/${address}`,
+    swapLink: (address: string) => `https://exchange.idamurni.de/#/swap?outputCurrency=${address}`,
+  },
+];
